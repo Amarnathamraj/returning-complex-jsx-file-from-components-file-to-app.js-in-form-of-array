7c55bc818a0e9e75d55691461cac5a8f52355ff4
Amarnathamraj <amarnath987654@gmail.com>
2024-03-17 22:31:57 +0530

    first commit

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,13 @@
+function Expenseitem(){
+    return (
+        <div>
+            <div>Date</div>
+            <div>delhi</div>
+            <div>
+                <h2>title</h2>
+                <div>Amount</div>
+            </div>
+        </div>
+    );
+}
+export default Expenseitem;
